8bbe0816d84f436574a9919f2090125ff407818b
CodingManoj <CodingManoj@gmail.com>
2025-12-06 17:58:34 -0500

    Provisioning V1 Infra

--- roboshop-v1/modules/ec2.tf
+++ roboshop-v1/modules/ec2.tf
@@ -1,8 +1,15 @@
 resource "aws_instance" "main" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [var.security_group_id]
 
   tags = {
     Name = "${var.name}-${var.env}"
   }
+}
+
+variable "security_group_id" {
+  description = "The ID of the existing security group."
+  type        = string
+  default     = "sg-057e0b9b259713373"
 }
