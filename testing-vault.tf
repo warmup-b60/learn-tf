a418812ca8e82ad23be00adc31c98a36f0d5f6be
CodingManoj <CodingManoj@gmail.com>
2025-12-14 18:08:41 -0500

    Roboshop V1: TF Infra + CM with Ansible is working - Secrets has to be extracted for systemd and app resource

---
+++ testing-vault.tf
@@ -0,0 +1,17 @@
+provider "vault" {
+  address = "http://172.31.67.238:8200"
+  token   = var.token # Use the provided token for authentication
+}
+
+variable "token" {
+    type    = string
+    default = "your-vault-token"
+}
+
+data "vault_generic_secret" "rundeck_auth" {
+  path = "kv/demo"
+}
+
+output "secret" {
+    value = data.vault_generic_secret.rundeck_auth.data   
+}
