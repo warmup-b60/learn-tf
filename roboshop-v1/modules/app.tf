da61f2ff93fa2e9ee4546a95add396bf32c76b67
CodingManoj <CodingManoj@gmail.com>
2025-12-14 22:12:54 -0500

    Roboshop V1: TF Infra + CM with Ansible is working - Secrets has to be extracted for systemd and app resource

--- roboshop-v1/modules/app.tf
+++ roboshop-v1/modules/app.tf
@@ -11,10 +11,7 @@ resource "null_resource" "app" {
     }
 
     inline = [
-      var.env != "" ?
-      "ansible-pull -i localhost, -U https://github.com/warmup-b60/roboshop-ansible.git -e component=${var.name} -e env=${var.env} main.yml"
-      :
-      "ansible-pull -i localhost, -U https://github.com/warmup-b60/roboshop-ansible.git -e component=${var.name} main.yml"
+      var.env == "" ? "ansible-pull -i localhost, -U https://github.com/warmup-b60/roboshop-ansible.git -e component=${var.name} main.yml" : "ansible-pull -i localhost, -U https://github.com/warmup-b60/roboshop-ansible.git -e component=${var.name} -e env=${var.env} main.yml"
     ]
   }
 }
