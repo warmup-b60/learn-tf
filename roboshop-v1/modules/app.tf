5dcb9225bdfd24cad858729fc29744382a5e77ff
CodingManoj <CodingManoj@gmail.com>
2025-12-14 19:18:59 -0500

    Roboshop V1: TF Infra + CM with Ansible is working - Secrets has to be extracted for systemd and app resource

---
+++ roboshop-v1/modules/app.tf
@@ -0,0 +1,19 @@
+resource "null_resource" "app" {
+  depends_on = [aws_route53_record.roboshop]
+  triggers = {
+    always = timestamp()
+  }
+  provisioner "remote-exec" { # This let's the execution to happen on the remote node
+    connection {
+      host     = aws_instance.main.private_ip
+      user     = "ec2-user"  # data.vault_generic_secret.ssh.data["username"]
+      password = "DevOps321" # data.vault_generic_secret.ssh.data["password"]
+    }
+
+    inline = [
+      "sudo pip3.11 install ansible",
+      "pip3.11 install hvac",
+      "ansible-pull  -i localhost, -U https://github.com/warmup-b60/roboshop-ansible.git -e component=${var.name} -e env=${var.env}  main.yml"
+    ]
+  }
+}
