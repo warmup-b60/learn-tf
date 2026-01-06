7f2fac40b9707f0be2ccd66e15e0c8276730c0a6
CodingManoj <CodingManoj@gmail.com>
2026-01-06 17:48:02 -0500

    Roboshop V1: TF Infra + CM with Ansible is working - Secrets has to be extracted for systemd and app resource

--- roboshop-v1/main.tf
+++ roboshop-v1/main.tf
@@ -1,18 +1,18 @@
-module "ec2" {
-  for_each = var.instances
+# module "ec2" {
+#   for_each = var.instances
 
-  source        = "./modules/ec2"
-  ami_id        = var.ami_id
-  instance_type = each.value["instance_type"]
-  env           = var.env
-  name          = each.key
-  zone_id       = var.zone_id
-  zone_name     = var.zone_name
-  token         = var.token
-}
+#   source        = "./modules/ec2"
+#   ami_id        = var.ami_id
+#   instance_type = each.value["instance_type"]
+#   env           = var.env
+#   name          = each.key
+#   zone_id       = var.zone_id
+#   zone_name     = var.zone_name
+#   token         = var.token
+# }
 
 
 
 module "eks" {
-  source        = "./modules/eks"
+  source = "./modules/eks"
 }
