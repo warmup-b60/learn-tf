a408e80cf7486c65ab73b9805ed75b9eef96ef9c
CodingManoj <CodingManoj@gmail.com>
2026-01-06 17:52:14 -0500

    Roboshop V1: TF Infra + CM with Ansible is working - Secrets has to be extracted for systemd and app resource

--- roboshop-v1/modules/eks/iam.tf
+++ roboshop-v1/modules/eks/iam.tf
@@ -27,11 +27,6 @@ resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSVPCResourceControlle
   role       = aws_iam_role.cluster.name
 }
 
-resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSClusterPolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.cluster.name
-}
-
 resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSComputePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSComputePolicy"
   role       = aws_iam_role.cluster.name
