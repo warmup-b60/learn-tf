8da3576ef9caed38112882f002490d8ff019a2c1
CodingManoj <CodingManoj@gmail.com>
2026-01-06 20:22:32 -0500

    Parameterizing the EKS Module

--- roboshop-v1/modules/eks/outputs.tf
+++ roboshop-v1/modules/eks/outputs.tf
@@ -1,7 +1,7 @@
 output "cluster_endpoint" {
-  value = aws_eks_cluster.example.endpoint
+  value = aws_eks_cluster.main.endpoint
 }
 
 output "kubeconfig_certificate_authority_data" {
-  value = aws_eks_cluster.example.certificate_authority[0].data
+  value = aws_eks_cluster.main.certificate_authority[0].data
 }
