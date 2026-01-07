48f983a359c95183048debb1298fc4c47ab5baba
CodingManoj <CodingManoj@gmail.com>
2026-01-06 21:48:43 -0500

    Roboshop V1: TF Infra + CM with Ansible is working - Secrets has to be extracted for systemd and app resource

--- roboshop-v1/modules/eks/nodeGroups.tf
+++ roboshop-v1/modules/eks/nodeGroups.tf
@@ -23,33 +23,3 @@ resource "aws_eks_node_group" "nodegroup_01" {
     aws_iam_role_policy_attachment.ng01-AmazonEC2ContainerRegistryReadOnly,
   ]
 }
-
-resource "aws_iam_role" "nodegroup_01_role" {
-  name = "${var.eks_nodegroup_01_role_name}-${var.env}"
-
-  assume_role_policy = jsonencode({
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = {
-        Service = "ec2.amazonaws.com"
-      }
-    }]
-    Version = "2012-10-17"
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "ng01-AmazonEKSWorkerNodePolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = aws_iam_role.nodegroup_01_role.name
-}
-
-resource "aws_iam_role_policy_attachment" "ng01-AmazonEKS_CNI_Policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = aws_iam_role.nodegroup_01_role.name
-}
-
-resource "aws_iam_role_policy_attachment" "ng01-AmazonEC2ContainerRegistryReadOnly" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = aws_iam_role.nodegroup_01_role.name
-}
