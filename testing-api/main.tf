d13f8bef6abb41b88f02f72c080392156b9b14a6
CodingManoj <CodingManoj@gmail.com>
2026-01-24 22:37:07 -0500

    Roboshop V1: TF Infra + CM with Ansible is working - Secrets has to be extracted for systemd and app resource

--- testing-api/main.tf
+++ testing-api/main.tf
@@ -1,5 +1,14 @@
-resource "null_resource" "call_httpbin" {
-  provisioner "local-exec" {
-    command = "curl -s https://httpbin.org/get | jq -r  '.origin' > httpbin_output.json"
-  }
+# resource "null_resource" "call_httpbin" {
+#   provisioner "local-exec" {
+#     command = "curl -s https://httpbin.org/get | jq -r  '.origin' > httpbin_output.json"
+#   }
+# }
+
+
+data "external" "httpbin_origin" {
+  program = ["bash", "-c", "curl -s https://httpbin.org/get | jq -r '.origin' | jq -Rn '{origin: input}'"]
+}
+
+output "origin" {
+  value = data.external.httpbin_origin.result.origin
 }
