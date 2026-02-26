320188149b0cf7c5a2f5a518c0c1bdc5e828bb15
CodingManoj <CodingManoj@gmail.com>
2026-02-25 21:55:08 -0500

    Provisioning EKS & DB's on EC2 for roboshop app on eks

--- roboshop-v1/main.tf
+++ roboshop-v1/main.tf
@@ -13,14 +13,14 @@ module "ec2" {
 }
 
 # Module that provisions the EKS Cluster and Nodegroup
-module "eks" {
-  source                     = "./modules/eks"
-  env                        = var.env
-  cluster_name               = var.cluster_name
-  eks_version                = var.eks_version
-  subnet_ids                 = var.subnet_ids
-  eks_cluster_role_name      = var.eks_cluster_role_name
-  eks_nodegroup_01_role_name = var.eks_nodegroup_01_role_name
-  nodegroup_01_name          = var.nodegroup_01_name
-  ng01_instance_types        = var.ng01_instance_types
-}
+# module "eks" {
+#   source                     = "./modules/eks"
+#   env                        = var.env
+#   cluster_name               = var.cluster_name
+#   eks_version                = var.eks_version
+#   subnet_ids                 = var.subnet_ids
+#   eks_cluster_role_name      = var.eks_cluster_role_name
+#   eks_nodegroup_01_role_name = var.eks_nodegroup_01_role_name
+#   nodegroup_01_name          = var.nodegroup_01_name
+#   ng01_instance_types        = var.ng01_instance_types
+# }
