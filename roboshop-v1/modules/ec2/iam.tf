30e09beb6c77e8048403e2b25a9c4e68939d119d
CodingManoj <CodingManoj@gmail.com>
2026-03-20 22:03:21 -0400

    Adding Actions

---
+++ roboshop-v1/modules/ec2/iam.tf
@@ -0,0 +1,20 @@
+resource "aws_iam_role" "main" {
+  name = "${var.name}-${var.env}-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "ec2.amazonaws.com"
+        },
+        "Action" : "sts:AssumeRole"
+      },
+    ]
+  })
+
+  tags = {
+    tag-key = "${var.name}-${var.env}-role"
+  }
+}
